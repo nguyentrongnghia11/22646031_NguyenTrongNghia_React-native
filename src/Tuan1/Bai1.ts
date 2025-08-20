fc0d90d59ae264fd952edd23632d33fa4d62e1f0
H82M19\Student <Student>
2025-08-20 07:47:51 +0700

    Bai 1 cua tuan 1

---
+++ src/Tuan1/Bai1.ts
@@ -0,0 +1,14 @@
+class Person {
+    constructor(name: string, age: number) {
+        this.name = name,
+            this.age = age
+    }
+    name: string;
+    age: number
+}
+
+
+
+const person = new Person ("Nguyen Trong Nghia", 21)
+
+console.log (person)
