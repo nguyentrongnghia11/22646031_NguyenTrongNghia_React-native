7a63534055ccfc2ac2d5e2d0f4406a512f86da02
nghia1501 <nghianguyen15012004@gmail.com>
2025-08-30 11:30:08 +0700

    bai 30 cua tuan 2

---
+++ dist/Tuan2/Bai30.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+async function fetchTodo(id) {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+}
+async function fetchMultipleTodos() {
+    const ids = [1, 2, 9999];
+    const results = await Promise.allSettled(ids.map(id => fetchTodo(id)));
+    results.forEach((result, index) => {
+        if (result.status === "fulfilled") {
+            console.log(`✅ Todo ${ids[index]} thành công:`, result.value);
+        }
+        else {
+            console.log(`❌ Todo ${ids[index]} thất bại:`, result.reason.message);
+        }
+    });
+}
+fetchMultipleTodos();
