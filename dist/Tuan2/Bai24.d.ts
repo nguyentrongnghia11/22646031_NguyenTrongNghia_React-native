7473f95fea172d3d0ef22d60fcf7ddd463db7d7f
nghia1501 <nghianguyen15012004@gmail.com>
2025-08-30 10:45:50 +0700

    bai 24 cua tuan 2

---
+++ dist/Tuan2/Bai24.d.ts
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=Bai24.d.ts.map
---
+++ dist/Tuan2/Bai24.js
@@ -0,0 +1,26 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+async function postData() {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                title: "Hello",
+                body: "This is a test post",
+                userId: 1
+            })
+        });
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        console.log("Kết quả POST:", data);
+    }
+    catch (err) {
+        console.error("Lỗi:", err);
+    }
+}
+postData();
