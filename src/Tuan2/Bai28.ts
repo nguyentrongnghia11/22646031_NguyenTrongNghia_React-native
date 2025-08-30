6884d414927f457b216a6aae78d6a78359dedb16
nghia1501 <nghianguyen15012004@gmail.com>
2025-08-30 11:12:42 +0700

    bai 28 cua tuan 2

---
+++ src/Tuan2/Bai28.ts
@@ -0,0 +1,21 @@
+async function fakeTask(id: number) {
+    const delay = Math.floor(Math.random() * 2000) + 1000; // 1-3 giây
+    await new Promise(resolve => setTimeout(resolve, delay));
+    console.log(`Task ${id} done`);
+    return id;
+}
+
+async function batchProcess() {
+    const tasks = [
+        fakeTask(1),
+        fakeTask(2),
+        fakeTask(3),
+        fakeTask(4),
+        fakeTask(5)
+    ];
+
+    const results = await Promise.all(tasks);
+    console.log("Tất cả task xong:", results);
+}
+
+batchProcess();
